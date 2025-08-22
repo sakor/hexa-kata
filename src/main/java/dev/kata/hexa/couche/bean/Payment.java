555509888524f3499d0097ec140ce05e4d29f0a3
sakor <romain.blas@gmail.com>
2025-08-22 14:20:51 +0200

    Itération 1 : Simples boissons, test et RG

---
+++ src/main/java/dev/kata/hexa/couche/bean/Payment.java
@@ -0,0 +1,10 @@
+package dev.kata.hexa.couche.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Payment {
+    private double amount;
+}
---
+++ src/test/java/dev/kata/hexa/couche/CoffeMachineServiceTest.java
@@ -0,0 +1,67 @@
+package dev.kata.hexa.couche;
+
+import dev.kata.hexa.couche.bean.Boisson;
+import dev.kata.hexa.couche.bean.Payment;
+import dev.kata.hexa.couche.dao.BoissonDAO;
+import dev.kata.hexa.couche.service.CoffeeMachineService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CoffeMachineServiceTest {
+
+    @Mock
+    private BoissonDAO boissonDAO;
+
+    @InjectMocks
+    private CoffeeMachineService service;
+
+    @Test
+    void orderDrink_WITH_the_and_2_euros_SHOULD_dispense_drink() {
+        // Given
+        var nomBoisson = "the";
+        var montant = 2.0;
+        when(boissonDAO.findByName(nomBoisson)).thenReturn(new Boisson(nomBoisson, montant));
+
+        // When
+        var result = service.orderDrink(nomBoisson, new Payment(2.0));
+
+        // Then
+        assertThat(result).isEqualTo("Voici votre " + nomBoisson);
+    }
+
+    @Test
+    void orderDrink_WITH_the_and_1_euros_SHOULD_not_dispense_drink() {
+        // Given
+        var nomBoisson = "the";
+        var montant = 2.0;
+        when(boissonDAO.findByName(nomBoisson)).thenReturn(new Boisson(nomBoisson, montant));
+
+        // When
+        var result = service.orderDrink(nomBoisson, new Payment(1.0));
+
+        // Then
+        assertThat(result).isEqualTo("Montant insuffisant (Prix : " + montant + "€)");
+    }
+
+    @Test
+    void orderDrink_WITH_unknow_and_2_euros_SHOULD_send_bad_drink_message() {
+        // Given
+        var nomBoisson = "unknow";
+        when(boissonDAO.findByName(nomBoisson)).thenReturn(null);
+
+        // When
+        var result = service.orderDrink(nomBoisson, new Payment(2.0));
+
+        // Then
+        assertThat(result).isEqualTo("Boisson inconnue.");
+    }
+
+
+}
