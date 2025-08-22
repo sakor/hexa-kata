76bab7076a769672bacd323f38b4cb723644a3d8
sakor <romain.blas@gmail.com>
2025-08-22 14:24:27 +0200

    Create Boison.java

---
+++ src/main/java/couche/bean/Boison.java
@@ -0,0 +1,14 @@
+public class Boison {
+  
+    private String name;
+    private double price;
+
+    public Boison(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+  
+}
